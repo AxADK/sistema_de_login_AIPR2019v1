e7b17448549a62876ab42f33131918ebcef2b587
AXaDK <alexandrekerber23@outlook.com>
2019-08-30 16:31:24 -0300

    Cadastro de usuário
    closes #8

--- recebe.php
+++ recebe.php
@@ -20,14 +20,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'cadastrar'){
     $concordar = $_POST['concordar'];
     $dataCriacao = date("Y-m-d H:i:s");
 
-    echo "<h5>Nome Completo: $nomeCompleto</h5>";
-    echo "<h5>Nome Usuario: $nomeUsuario</h5>";
-    echo "<h5>E-mail Usuario: $emailUsuario</h5>";
-    echo "<h5>Senha Usuario: $senhaUsuario</h5>";
-    echo "<h5>Senha Confirma: $senhaConfirma</h5>";
-    echo "<h5>Concordar: $concordar</h5>";
-    echo "<h5>Data de Criação: $dataCriacao</h5>";
-
+    //hash de senha / codificação de senha em 40 carácteres
     $senha = sha1($senhaUsuario);
     $senhaC = sha1($senhaConfirma);
 
@@ -35,9 +28,28 @@ if(isset($_POST['action']) && $_POST['action'] == 'cadastrar'){
         echo "<h1>As senhas não conferem</h1>";
         exit();
     }else{
-        echo "<h5>senha codificada: $senha</h5>";
+        //echo "<h5>senha codificada: $senha</h5>";
+        //Verificar se o usuário ja existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR  email = ?");
+        //Substitui cada ? por uma string abaixo
+        $sql->bind_param("ss",$nomeUsuario, $emailUsuario);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        if($linha['nomeUsuario'] == $nomeUsuario){
+            echo "<p>Nome de usuário indisponível, tente outro</p>";
+        }elseif ($linha['email'] == $emailUsuario) {
+            echo "<p>E-mail já em uso, tente outro</p>";
+        }else{
+            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, dataCriacao) values(?, ?, ?, ?, ?)");
+            $sql->bind_param("sssss",$nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $dataCriacao);
+            if($sql->execute()){
+                echo "<p>Registrado com sucesso</p>";
+            }else{
+                echo "<p>Algo deu errado. Tente ou vez.</p>";
+            }
+        }
     }
-
 }else{
     echo "<h1 style='color:red'>Esta página não pode ser acessada diretamente</h1>";
 }
