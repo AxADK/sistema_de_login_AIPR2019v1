7f6022e838ca1039d1b7b4acd8cee12698ce806c
alexandre kerber <alexandre_kerber@estudante.sc.senai.br>
2019-09-19 15:04:49 -0300

    Geração de Token
    1° passo para criar uma nova senha, gerar um token

--- recebe.php
+++ recebe.php
@@ -10,7 +10,30 @@ function verificar_entrada($entrada)
     $saida = htmlspecialchars($saida);
     return $saida;
 }
-if (isset($_POST['action']) && $_POST['action'] == 'login') {
+
+if(isset($_POST['action']) && $_POST['action'] == 'senha'){
+    //Apenas para Debug/ Teste
+    //echo "<strong>Recuperação de senha</stron";
+
+    $emailSenha = verificar_entrada($_POST['emailSenha']);
+    $sql = $conecta->prepare("SELECT idUsuario FROM usuario WHERE email = ?");
+    $sql->bind_param("s", $emailSenha);
+    $sql->execute();
+    $resultado = $sql->get_result();
+    if($resultado->num_rows > 0){
+        //Existe o usuario no banco de dados
+        //Só para testar / Debug
+        //echo "<p class=\"text-success\">E-mail encontrado</p>";
+        $frase = "BatAtinh4Quando!@#$%NAscexexeEsparr4ma4PeloChao&*";
+        $frase_secreta = str_shuffle($frase);
+        $token = substr($frase_secreta,0,10);
+        echo "<p>$token</p>";
+    }else{
+        echo '<p class="text-danger">E-mail não encontrado</p>';
+
+    }
+
+}else if(isset($_POST['action']) && $_POST['action'] == 'login'){
     //Verificação e Login do usuário
     $nomeUsuario = verificar_entrada($_POST['nomeUsuario']);
     $senhaUsuario = verificar_entrada($_POST['senhaUsuario']);
@@ -68,6 +91,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'login') {
         $sql->execute();
         $resultado = $sql->get_result();
         $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        
         if ($linha['nomeUsuario'] == $nomeUsuario) {
             echo "<p>Nome de usuário indisponível, tente outro</p>";
         } elseif ($linha['email'] == $emailUsuario) {
